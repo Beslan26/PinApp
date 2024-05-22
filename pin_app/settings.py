46cb9fa1894553df9abf26b8414775376636526c
Beslan <beslan.shavtikov@mail.ru>
2024-05-22 18:08:10 +0300

    Настройка подключения к базе данных

--- pin_app/settings.py
+++ pin_app/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'pin_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'pin_app',
+        'USER': 'postgres',
+        'PASSWORD': '026',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
